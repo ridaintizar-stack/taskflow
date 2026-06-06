5a6c1506f01fd1603d9de4fd23995e532c5e0fca
ridaintizar-stack <ridaintizar@gmail.com>
2026-06-06 06:40:21 -0600

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://sfmoolttsjtjihdpizav.supabase.co'
-const supabaseKey = 'sb_publishable_S_ZHJwFDRQ4E8wJE2bCcvw_dGiRLsNK'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmbW9vbHR0c2p0amloZHBpemF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3Mzc1OTcsImV4cCI6MjA5NjMxMzU5N30.rTdsveDH5kbrMeGsjotNh6DoDNTOkZMmMnkNjIzLyjI'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
